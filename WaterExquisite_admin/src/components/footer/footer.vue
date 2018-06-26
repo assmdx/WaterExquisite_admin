d13059914db39183aab9580df6c177bae4a32613
renzhenchuan <renzhenchuan@internetware.cn>
2018-06-26 17:44:00 +0800

    add 添加footer.vue和路由处理

---
+++ WaterExquisite_admin/src/components/footer/footer.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item " v-bind:class="{mui-active:isGoodManage}" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">商品管理</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" v-bind:class="{mui-active:isOrderManaga}" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">订单管理</span>
+			</a>
+
+		</nav>
+</template>
+
+<script>
+export default {
+  data() {},
+  props: ['title'],
+  computed: {
+    isGoodManage() {
+      if (this.title === 'good' || this.title === 'addGood') {
+        return true
+      }
+    },
+    isOrder() {
+      if (this.title === 'order') {
+        return true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- WaterExquisite_admin/src/router/router.js
+++ WaterExquisite_admin/src/router/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: 'App',
+      children:[{
+        path:'',
+        component:r => require.ensure([], () => r(require('../page/good')), 'good'),
+        children:[{
+          path:'/addGood',
+          component:r => require.ensure([], () => r(require('../page/good/children/addGood')), 'addGood')
+        }]
+      },{
+        path:'order',
+        component:component: r => require.ensure([], () => r(require('../page/order')), 'order')
+      }]
     }
   ]
 })
