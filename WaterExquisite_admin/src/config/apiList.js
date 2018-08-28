b3e54a6a51916bcfe0c951e52a5a4f1739e66d2a
assmdx <assmdx183@163.com>
2018-08-28 15:07:07 +0800

    UPDATE 修改文件服务器和后台对应的url

--- WaterExquisite_admin/src/config/apiList.js
+++ WaterExquisite_admin/src/config/apiList.js
@@ -1,10 +1,10 @@
-const ApiRootUrl = 'http://127.0.0.1:8360/WaterExquisite_manage/admin/'
+const ApiRootUrl = '../WaterExquisite_manage/admin/'
 let GoodsList = ApiRootUrl + 'goods/index' //获得商品列表
 let GoodAdd = ApiRootUrl + 'goods/add'  //添加商品
 let GoodDelete = ApiRootUrl + 'goods/delete' //删除商品
 
-const FileServerUrl = 'http://127.0.0.1:8089/';
-const FileServerToken = '';
+const FileServerUrl = '../WaterExquisite_FileServer/';
+const FileServerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFwcG1sZXgiLCJuYW1lIjoic2l6aGFpIiwiaWF0IjoxNTM0MDgwNjA5fQ.iuhjnNu4PKtck7IIM01MQhe6xRdrUaymnac7MgZwRw0';
 let addImage = FileServerUrl + 'add';
 let changeImage = FileServerUrl + 'change';
 let getImage = FileServerUrl + 'get';
